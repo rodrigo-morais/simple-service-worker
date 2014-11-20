67c0d8a660642031ebc5f780171dc595dcd75c04
Rodrigo Morais <morais.rm@gmail.com>
2014-11-20 10:43:39 -0200

    Create repository.

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+﻿if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("javascript/service-worker.js").then(function (serviceWorker) {
+        document.querySelector('#serviceWorkerState').innerHTML = "Registered";
+    }).catch(function (error) {
+        document.querySelector('#serviceWorkerState').textContent = error;
+    });
+}
+else {
+    document.querySelector('#serviceWorkerState').textContent = 'Unregistered';
+}
